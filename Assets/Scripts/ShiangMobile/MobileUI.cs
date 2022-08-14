41efc22cfe2379c983c64a7e3ffd7840dbaf4c8b
Bowen Zhang <chambowen@126.com>
2022-08-14 20:55:08 +0800

    move to Shiang namespace

--- Assets/Scripts/ShiangMobile/MobileUI.cs
+++ Assets/Scripts/ShiangMobile/MobileUI.cs
@@ -2,14 +2,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MobileUI : MonoBehaviour
+namespace Shiang
 {
-    [SerializeField] public Button MOBILE_Left;
-    [SerializeField] public Button MOBILE_Right;
-    [SerializeField] public Button MOBILE_Interact;
-    [SerializeField] public Button MOBILE_Ability;
-    [SerializeField] public Button MOBILE_Weapon;
-    [SerializeField] public Button MOBILE_Zoom;
-    [SerializeField] public Button MOBILE_Switch;
-    [SerializeField] public Button MOBILE_Esc;
+    public class MobileUI : MonoBehaviour
+    {
+        [SerializeField] public Button MOBILE_Left;
+        [SerializeField] public Button MOBILE_Right;
+        [SerializeField] public Button MOBILE_Interact;
+        [SerializeField] public Button MOBILE_Ability;
+        [SerializeField] public Button MOBILE_Weapon;
+        [SerializeField] public Button MOBILE_Zoom;
+        [SerializeField] public Button MOBILE_Switch;
+        [SerializeField] public Button MOBILE_Esc;
+    }
 }
